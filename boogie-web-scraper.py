bc229b7bc4f92ed297ec7ba1baaf93061a5b6637
TheBigRedDog <clmmoran@ucdavis.edu>
2022-05-08 16:48:10 -0700

    Updated web scraper to navigate to uspa.org events page

--- boogie-web-scraper.py
+++ boogie-web-scraper.py
@@ -9,3 +9,6 @@ options.add_argument("window-size=1920,1200")
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 
 driver.get('https://uspa.org/Events')
+
+boogie_option = driver.find_element_by_xpath('//input[id="BO"][name="BO"][type="checkbox"]')
+boogie_option.click()
