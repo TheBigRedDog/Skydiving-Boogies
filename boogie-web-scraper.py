8c2594c5e9e5dc483e4422342325b1c634a3b6b0
TheBigRedDog <clmmoran@ucdavis.edu>
2022-05-09 12:49:44 -0700

    refactored bs4 parsing into separate functions for each type of information about a boogie

--- boogie-web-scraper.py
+++ boogie-web-scraper.py
@@ -24,47 +24,101 @@ find_events_button.click()
 time.sleep(2)
 
 boogies = driver.find_elements(by=By.CLASS_NAME, value='mx-template.mx-product-details-template')
-# TODO: IMPLEMENT SEPARATE PARSING FUNCTIONS FOR BOOGIE CONTACT INFO IN CASE IT IS ABSENT IN FUTURE BOOGIE ENTRIES
-def parse_boogie_contact_info(boogie_soup_object, boogie_info):
-    boogie_contact_info = boogie_soup.find(class_='col-sm-4')
-    counter = 0
-    boogie_dropzone = boogie_contact_info.find(class_='fa fa-plane')\
-        .next_sibling.next_sibling
-    boogie_location = boogie_contact_info.find(class_='fa fa-plane')\
-        .next_sibling.next_sibling.next_sibling.split()
-    boogie_main_contact = boogie_contact_info.find(class_='fa fa-user')\
-        .next_sibling.text.strip().split(', ')[0]
-    boogie_phone_number = boogie_contact_info.find(class_='fa fa-phone')
-    if boogie_phone_number != None:
-        boogie_phone_number = boogie_phone_number\
-            .next_sibling.next_sibling.text.strip()
-    boogie_email = boogie_contact_info.find(class_='fa fa-envelope')\
-        .next_sibling.next_sibling.text.strip()
-    boogie_link = boogie_contact_info.find(class_='fa fa-external-link')\
-        .next_sibling.next_sibling.text.strip()
-    print(boogie_link)
-    # for child in boogie_contact_info.children:
-    #     if len(child.find_all)
-    #     text = child.text.strip()
-    #     print(text)
 
-    return boogie_contact_info
+def parse_boogie_dropzone(boogie_soup_object, boogie_info):
+    boogie_dropzone_sibling = boogie_soup_object.find(class_='col-sm-4').find(class_='fa fa-plane')
+    if boogie_dropzone_sibling != None:
+        boogie_dropzone = boogie_dropzone_sibling\
+            .next_sibling.next_sibling.text
+        boogie_info['dropzone'] = boogie_dropzone
+    else:
+        boogie_info['dropzone'] = None
+
+def parse_boogie_location(boogie_soup_object, boogie_info):
+    boogie_location_sibling = boogie_soup_object.find(class_='col-sm-4').find(class_='fa fa-plane')
+    if boogie_location_sibling != None:
+        boogie_location = boogie_location_sibling\
+            .next_sibling.next_sibling.next_sibling.split()
+        boogie_info['city'] = boogie_location[0]
+        boogie_info['state'] = boogie_location[1]
+    else:
+        boogie_info['city'] = None
+        boogie_info['state'] = None
+
+def parse_boogie_main_contact(boogie_soup_object, boogie_info):
+    boogie_main_contact_sibling = boogie_soup_object.find(class_='col-sm-4').find(class_='fa fa-user')
+    if boogie_main_contact_sibling != None:
+        boogie_main_contact = boogie_main_contact_sibling\
+            .next_sibling.text.strip().split(', ')[0]
+        boogie_info['main_contact'] = boogie_main_contact
+    else:
+        boogie_info['main_contact'] = None
+
+def parse_boogie_phone_number(boogie_soup_object, boogie_info):
+    boogie_phone_number_sibling = boogie_soup_object.find(class_='col-sm-4').find(class_='fa fa-phone')
+    if boogie_phone_number_sibling != None:
+        boogie_phone_number = boogie_phone_number_sibling\
+            .next_sibling.next_sibling.text.strip()
+        boogie_info['boogie_phone_number'] = boogie_phone_number
+    else:
+        boogie_info['boogie_phone_number'] = None
+    
+def parse_boogie_email(boogie_soup_object, boogie_info):
+    boogie_email_sibling = boogie_soup_object.find(class_='col-sm-4').find(class_='fa fa-envelope')
+    if boogie_email_sibling != None:
+        boogie_email = boogie_email_sibling\
+            .next_sibling.next_sibling.text.strip()
+        boogie_info['email'] = boogie_email
+    else:
+        boogie_info['email'] = None
+
+def parse_boogie_link(boogie_soup_object, boogie_info):
+    boogie_link_sibling = boogie_soup_object.find(class_='col-sm-4').find(class_='fa fa-external-link')
+    if boogie_link_sibling != None:
+        boogie_link = boogie_link_sibling\
+            .next_sibling.next_sibling.text.strip()
+        boogie_info['link'] = boogie_link
+    else:
+        boogie_info['link'] = None
+
+def parse_boogie_name(boogie_soup_object, boogie_info):
+    boogie_name_parent = boogie_soup_object.find(class_='col-sm-7')
+    if boogie_name_parent != None:
+        boogie_name = boogie_name_parent\
+            .contents[1].text.strip()
+        boogie_info['name'] = boogie_name
+    else:
+        boogie_info['name'] = None
+
+def parse_boogie_dates(boogie_soup_object, boogie_info):
+    boogie_dates_parent = boogie_soup_object.find(class_='col-sm-5')
+    if boogie_dates_parent != None:
+        boogie_dates = boogie_dates_parent\
+            .contents[1].contents[1].contents[2]\
+            .text.strip().replace("\n", "").split(' - ')
+        boogie_info['start_date'] = boogie_dates[0]
+        boogie_info['end_date'] = boogie_dates[1]
+    else:
+        boogie_info['start_date'] = None
+        boogie_info['end_date'] = None
 
 for boogie in boogies:
     boogie_info = {}
     boogie_html = boogie.get_attribute('outerHTML')
     boogie_soup = BeautifulSoup(boogie_html, 'html5lib')
     
-    boogie_info['name'] = boogie_soup.find(class_='col-sm-7').contents[1].text.strip()
-    boogie_info['dates'] = boogie_soup.find(class_='col-sm-5')\
-        .contents[1].contents[1].contents[2]\
-        .text.strip().replace("\n", "").split(' - ')
-    parse_boogie_contact_info(boogie_soup, boogie_info)
+    parse_boogie_name(boogie_soup, boogie_info)
+    parse_boogie_dates(boogie_soup, boogie_info)
+    parse_boogie_dropzone(boogie_soup, boogie_info)
+    parse_boogie_location(boogie_soup, boogie_info)
+    parse_boogie_main_contact(boogie_soup, boogie_info)
+    parse_boogie_phone_number(boogie_soup, boogie_info)
+    parse_boogie_email(boogie_soup, boogie_info)
+    parse_boogie_link(boogie_soup, boogie_info)
+    print(boogie_info)
+
+    
   
-    #1 = location
-    #3 = main contact
-    #
-    #4
 
 
 
