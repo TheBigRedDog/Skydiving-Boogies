228379cd52d203444720793aa1d930664858a3f5
TheBigRedDog <clmmoran@ucdavis.edu>
2022-05-08 21:39:33 -0700

    added bs4 parsing of boogie dz, location, phone #

--- boogie-web-scraper.py
+++ boogie-web-scraper.py
@@ -21,22 +21,51 @@ boogie_option.click()
 
 find_events_button = driver.find_element(by=By.XPATH, value='/html/body/form/div[4]/section[2]/div/div/div[10]/div[1]/div/div/div/div/div/form/input')
 find_events_button.click()
-time.sleep(5)
+time.sleep(2)
 
 boogies = driver.find_elements(by=By.CLASS_NAME, value='mx-template.mx-product-details-template')
+# TODO: IMPLEMENT SEPARATE PARSING FUNCTIONS FOR BOOGIE CONTACT INFO IN CASE IT IS ABSENT IN FUTURE BOOGIE ENTRIES
+def parse_boogie_contact_info(boogie_soup_object, boogie_info):
+    boogie_contact_info = boogie_soup.find(class_='col-sm-4')
+    counter = 0
+    boogie_dropzone = boogie_contact_info.find(class_='fa fa-plane')\
+        .next_sibling.next_sibling
+    boogie_location = boogie_contact_info.find(class_='fa fa-plane')\
+        .next_sibling.next_sibling.next_sibling.split()
+    boogie_main_contact = boogie_contact_info.find(class_='fa fa-user')\
+        .next_sibling.text.strip().split(', ')[0]
+    boogie_phone_number = boogie_contact_info.find(class_='fa fa-phone')
+    if boogie_phone_number != None:
+        boogie_phone_number = boogie_phone_number\
+            .next_sibling.next_sibling.text.strip()
+    # TODO: implement scraping for email and event page link
+    # boogie_email= boogie_contact_info.find(class_='fa fa-envelope')\
+    #     .next_sibling.next_sibling.text.strip()
+    # print(boogie_email)
+    # for child in boogie_contact_info.children:
+    #     if len(child.find_all)
+    #     text = child.text.strip()
+    #     print(text)
+
+    return boogie_contact_info
 
 for boogie in boogies:
-
+    boogie_info = {}
     boogie_html = boogie.get_attribute('outerHTML')
     boogie_soup = BeautifulSoup(boogie_html, 'html5lib')
-
-    boogie_name = boogie_soup.find(class_='col-sm-7').contents[1].text.strip()
-    boogie_dates = boogie_soup.find(class_='col-sm-5')\
+    
+    boogie_info['name'] = boogie_soup.find(class_='col-sm-7').contents[1].text.strip()
+    boogie_info['dates'] = boogie_soup.find(class_='col-sm-5')\
         .contents[1].contents[1].contents[2]\
         .text.strip().replace("\n", "").split(' - ')
-    
-    print(boogie_dates)
-    
+    parse_boogie_contact_info(boogie_soup, boogie_info)
+  
+    #1 = location
+    #3 = main contact
+    #
+    #4
+
+
 
 
 '''
